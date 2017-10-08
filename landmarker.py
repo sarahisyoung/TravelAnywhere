8afb41477076fd9653ddd2cd6225651caf16b1fe
Sarah Young <sarahyoung@Sarahs-MacBook-Pro.local>
2017-10-07 21:34:01 -0700

    landmarker function

---
+++ landmarker.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import io
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+
+
+# [START def_detect_landmarks]
+def detect_landmarks(path):
+    """Detects landmarks in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    # [START migration_landmark_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.landmark_detection(image=image)
+    landmarks = response.landmark_annotations
+    print('Landmarks:')
+
+    for landmark in landmarks:
+        print(landmark.description)
+        for location in landmark.locations:
+            lat_lng = location.lat_lng
+            print('Latitude'.format(lat_lng.latitude))
+            print('Longitude'.format(lat_lng.longitude))
+    # [END migration_landmark_detection]
+# [END def_detect_landmarks]
